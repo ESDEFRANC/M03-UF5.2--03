495779a04d164f70771eec74bcd8a85e987eeabd
esdefranc <Adriaaltemir18@gmail.com>
2018-10-27 16:17:14 +0200

    All
    All done

---
+++ src/m03/uf5/pkg2/pkg03/SaldoInsuficienteException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m03.uf5.pkg2.pkg03;
+
+/**
+ *
+ * @author usuario
+ */
+public class SaldoInsuficienteException extends Exception{
+    public SaldoInsuficienteException(String message){
+        super(message);
+    }
+    
+}
